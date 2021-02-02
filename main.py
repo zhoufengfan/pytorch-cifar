1c6cd5033182f666127c470c4e709ff0a6f6c35e
zhoufengfan <zhfengfan@126.com>
2021-02-02 17:50:39 +0800

    add transform_train transforms.ToPILImage()

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
+    transforms.ToPILImage(),
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
