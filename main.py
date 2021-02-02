10da5264307a8dab2ccac20cfdc31725b2ecea68
zhoufengfan <zhfengfan@126.com>
2021-02-02 19:52:53 +0800

    delete toPilimg in transform_train

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 #
 print('==> Preparing data..')
 transform_train = transforms.Compose([
-    transforms.ToPILImage(),
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
